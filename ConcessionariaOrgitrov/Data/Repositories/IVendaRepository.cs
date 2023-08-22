b47a80aac797564a8b51311fe7bd3a5d5912376f
wfmarques2 <wfmarques2@gmail.com>
2023-08-22 16:03:11 -0300

    Vendas_Servces_Repos

---
+++ ConcessionariaOrgitrov/Data/Repositories/IVendaRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ConcessionariaOrgitrov.Models;
+
+namespace ConcessionariaOrgitrov.Data.Repositories
+{
+    public interface IVendaRepository
+    {
+        IEnumerable<Venda> GetAllVendas();
+        Venda GetVendaById(int id);
+        void AddVenda(Venda venda);
+        void UpdateVenda(Venda venda);
+        void DeleteVenda(Venda venda);
+    }
+}
---
+++ ConcessionariaOrgitrov/Services/VendaService.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using ConcessionariaOrgitrov.Data.Repositories;
+using ConcessionariaOrgitrov.Models;
+
+namespace ConcessionariaOrgitrov.Services
+{
+    public class VendaService : IVendaService
+    {
+        private readonly IVendaRepository _vendaRepository;
+
+        public VendaService(IVendaRepository vendaRepository)
+        {
+            _vendaRepository = vendaRepository;
+        }
+
+        public IEnumerable<Venda> GetAllVendas()
+        {
+            return _vendaRepository.GetAllVendas();
+        }
+
+        public Venda GetVendaById(int id)
+        {
+            return _vendaRepository.GetVendaById(id);
+        }
+
+        public void AddVenda(Venda venda)
+        {
+            _vendaRepository.AddVenda(venda);
+        }
+
+        public void UpdateVenda(int id, Venda venda)
+        {
+            var existingVenda = _vendaRepository.GetVendaById(id);
+            if (existingVenda != null)
+            {
+                existingVenda.Cliente = venda.Cliente;
+                existingVenda.Carro = venda.Carro;
+                existingVenda.Valor = venda.Valor;
+                existingVenda.FormaPagamento = venda.FormaPagamento;
+
+                _vendaRepository.UpdateVenda(existingVenda);
+            }
+        }
+
+        public void DeleteVenda(int id)
+        {
+            var venda = _vendaRepository.GetVendaById(id);
+            if (venda != null)
+            {
+                _vendaRepository.DeleteVenda(venda);
+            }
+        }
+    }
+}
